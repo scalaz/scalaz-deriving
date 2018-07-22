97d7e5c26af73b1109a3d9808f64ede529a71860
Sam Halliday <sam.halliday@gmail.com>
2018-07-22 23:15:17 +0100

    clean the AST

--- examples/jsonformat/src/main/scala/jsonformat/JsValue.scala
+++ examples/jsonformat/src/main/scala/jsonformat/JsValue.scala
@@ -12,15 +12,10 @@ sealed abstract class JsValue {
   def widen: JsValue = this
 }
 
-@deriving(Monoid)
-case object JsNull extends JsValue
-@deriving(Monoid)
+case object JsNull                                          extends JsValue
 final case class JsObject(fields: IList[(String, JsValue)]) extends JsValue
-@deriving(Monoid)
-final case class JsArray(elements: IList[JsValue]) extends JsValue
-final case class JsBoolean(value: Boolean)         extends JsValue
-@deriving(Monoid)
-final case class JsString(value: String) extends JsValue
-final case class JsDouble(value: Double) extends JsValue
-@deriving(Monoid)
-final case class JsInteger(value: Long) extends JsValue
+final case class JsArray(elements: IList[JsValue])          extends JsValue
+final case class JsBoolean(value: Boolean)                  extends JsValue
+final case class JsString(value: String)                    extends JsValue
+final case class JsDouble(value: Double)                    extends JsValue
+final case class JsInteger(value: Long)                     extends JsValue
